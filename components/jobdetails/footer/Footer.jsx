5863d25ebb91346f38b8d4e3dfe8ca3b0d292043
Keane <keane@usernoMacBook-Pro.local>
2024-04-21 21:31:13 +0900

    like butotn

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity, Image, Linking } from "react-native";
 
 import styles from "./footer.style";
 import { icons } from "../../../constants";
 
 const Footer = ({ url }) => {
+  const [heart, setHeart] = useState(false);
+
+  const pressHeart = () => {
+    setHeart(!heart);
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.likeBtn}>
-        <Image source={icons.heartOutline} resizeMode="contain" style={styles.likeBtnImage} />
+      <TouchableOpacity style={styles.likeBtn} onPress={pressHeart}>
+        <Image
+          source={heart ? icons.heartFilled : icons.heartOutline}
+          resizeMode="contain"
+          style={styles.likeBtnImage}
+        />
       </TouchableOpacity>
       <TouchableOpacity style={styles.applyBtn} onPress={() => Linking.openURL(url)}>
         <Text style={styles.applyBtnText}>Apply For Job</Text>
